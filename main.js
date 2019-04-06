5f0af57c356b560fc4083de321db3c8c212a6fca
P4llix <45855475+P4llix@users.noreply.github.com>
2019-04-06 21:32:08 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
-function bg(){
-    
-}
+var d1 = document.getElementById("list-i");
+d1.style.background = "rgb(0,98,186)";
+d1.style.color = "white";
+
+
+document.getElementById("list-l").addEventListener("click", function(){
+
+    var d1 = document.getElementById("list-l");
+    d1.style.background = "rgb(0,98,186)";
+    d1.style.color = "white";
+
+    var d2 = document.getElementById("list-i");
+    d2.style.background = "white";
+    d2.style.color = "black";
+
+    var d3 = document.getElementById("list-r");
+    d3.style.background = "white";
+    d3.style.color = "black";
+
+    var d4 = document.getElementById("integ");
+    d4.innerText = "Każdy z nas ma możliwość realizować swoje zawodowe pasje, uczyć się od najlepszych, a przy tym współpracować z pozytywnymi, interesującymi ludźmi.";
+})
+
+
+document.getElementById("list-i").addEventListener("click", function(){
+
+    var d1 = document.getElementById("list-i");
+    d1.style.background = "rgb(0,98,186)";
+    d1.style.color = "white";
+
+    var d2 = document.getElementById("list-l");
+    d2.style.background = "white";
+    d2.style.color = "black";
+
+    var d3 = document.getElementById("list-r");
+    d3.style.background = "white";
+    d3.style.color = "black";
+
+    var d4 = document.getElementById("integ");
+    d4.innerText = "Integracja – na tym znamy się najlepiej. Lubimy ze sobą przebywać, ponieważ tworzymy zgrany zespół fajnych ludzi. W wolnym czasie spotykamy się, gramy w piłkę, żeglujemy, jeździmy na nartach… O ile tylko mamy na to ochotę.";
+})
+
+
+document.getElementById("list-r").addEventListener("click", function(){
+
+    var d1 = document.getElementById("list-r");
+    d1.style.background = "rgb(0,98,186)";
+    d1.style.color = "white";
+
+    var d2 = document.getElementById("list-i");
+    d2.style.background = "white";
+    d2.style.color = "black";
+
+    var d3 = document.getElementById("list-l");
+    d3.style.background = "white";
+    d3.style.color = "black";
+
+    var d4 = document.getElementById("integ");
+    d4.innerText = "Stwarzamy naszym pracownikom szanse na rozwijanie wiedzy, a także kompetencji handlowych i menedżerskich. Szkolenia, udział w innowacyjnych projektach, współpraca z profesjonalistami pozwala na nieustanne doskonalenie swoich umiejętności.";
+})
